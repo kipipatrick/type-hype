fe76c1c3d47fd5c937e589e1da9ccdfc5cc0c4ed
Kerwin Patrick <kipigo@Kerwins-MacBook-Pro.local>
2020-08-09 02:00:42 +0800

    Updates

---
+++ precache-manifest.7ddf6461a1c6c6d6600ba6707ff20ee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28303aa4e0326ea6cd36a48c0325bf8c",
+    "url": "/type-hype/index.html"
+  },
+  {
+    "revision": "ab4547380667afe68d94",
+    "url": "/type-hype/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "7cf2cade7135a0964b8f",
+    "url": "/type-hype/static/css/main.78797e22.chunk.css"
+  },
+  {
+    "revision": "ab4547380667afe68d94",
+    "url": "/type-hype/static/js/2.47f654c9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/type-hype/static/js/2.47f654c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf2cade7135a0964b8f",
+    "url": "/type-hype/static/js/main.a0eef2f7.chunk.js"
+  },
+  {
+    "revision": "67095a1ea728f11b6de5",
+    "url": "/type-hype/static/js/runtime-main.ccc44ed8.js"
+  }
+]);
