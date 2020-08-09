aa345ee31c08d5547a66faa6527e1ef73063a858
Kerwin Patrick <kipigo@Kerwins-MacBook-Pro.local>
2020-08-09 12:45:41 +0800

    Updates

--- precache-manifest.53698735ae22aae8cb328f5c84f45576.js
+++ precache-manifest.53698735ae22aae8cb328f5c84f45576.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b46d3b83363aa28881d0efb73d485328",
+    "revision": "24b092c30b2816aaa547e71583fd0feb",
     "url": "/type-hype/index.html"
   },
   {
-    "revision": "d1494f7e1e84d981a6ea",
+    "revision": "299749048a85b7083fd0",
     "url": "/type-hype/static/css/2.9c8c8187.chunk.css"
   },
   {
-    "revision": "f0dcdc7ccf0087f91464",
+    "revision": "0a07bc9144c779db1709",
     "url": "/type-hype/static/css/main.78797e22.chunk.css"
   },
   {
-    "revision": "d1494f7e1e84d981a6ea",
-    "url": "/type-hype/static/js/2.4f8a21f8.chunk.js"
+    "revision": "299749048a85b7083fd0",
+    "url": "/type-hype/static/js/2.8f184209.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/type-hype/static/js/2.4f8a21f8.chunk.js.LICENSE.txt"
+    "url": "/type-hype/static/js/2.8f184209.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f0dcdc7ccf0087f91464",
-    "url": "/type-hype/static/js/main.b56ca502.chunk.js"
+    "revision": "0a07bc9144c779db1709",
+    "url": "/type-hype/static/js/main.226e861b.chunk.js"
   },
   {
     "revision": "67095a1ea728f11b6de5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/type-hype/precache-manifest.0b9c207c5b849f1a54d5a15eebd43b30.js"
+  "/type-hype/precache-manifest.53698735ae22aae8cb328f5c84f45576.js"
 );
 
 self.addEventListener('message', (event) => {
